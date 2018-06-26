78c9ba33daa092704ba93b692bf5e05fa7b57130
theroadlesstraveledby <39222800+theroadlesstraveledby@users.noreply.github.com>
2018-06-26 22:52:38 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,42 @@
+
+const firstNames = ["Gandalf", "Saruman", "Merlin", "White-beard", "Stonehenge", "Catnip", "Albus", "Winstone", "Fluffy", "Albert", "Engelbert", "Sterheim", "Salamander", "Battlemeyer", "Baldwin", "Stanley", "Bartholomew", "Angleworth", "Northwind", "Melvin", "Trottleburt", "Shropshire", "Hemlock", "Appleby", "Beetle", "Anglomorous"];
+
+const lastNames = ["Great", "Skunk", "Humble", "Daft", "Toad", "Grim", "Whisk", "Whiz", "Turnip", "Wise", "String-bean", "Grey", "White", "Trustworthy", "Fool", "Dreadful", "Bewildered", "Brainy", "Abashed", "Cat", "Caffinated", "Jubilant", "Merry", "Meek", "Outlandish", "Quizzical"];
+
+function randomInt(limit) {
+  return Math.floor(Math.random() * limit);
+}
+
+function randomEntry(array) {
+  const randomIndex = randomInt(array.length);
+
+  return array[randomIndex];
+}
+
+function randomFirstName() {
+  return randomEntry(firstNames);
+}
+
+function randomLastName() {
+  return randomEntry(lastNames);
+}
+
+function randomName() {
+  const firstName = randomFirstName();
+  const lastName = randomLastName();
+  const fullName = firstName + ' the ' + lastName;
+
+  return fullName;
+}
+
+function nameGenerationHandler() {
+  const nameDiv = document.getElementById('name');
+  const newName = randomName();
+
+  nameDiv.textContent = newName;
+}
+
+document.getElementById('generate').addEventListener('click', nameGenerationHandler);
+
+  
+
